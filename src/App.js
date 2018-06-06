bb541ce6989eb54a621e6df0237e5a58c9c45427
Bruce Schubert <bruce@emxsys.com>
2018-06-06 03:42:27 -0700

    Cloned emxsys/worldwind-react-app

---
+++ src/App.js
@@ -0,0 +1,130 @@
+/* 
+ * Copyright (c) 2018 Bruce Schubert.
+ * The MIT License
+ * http://www.opensource.org/licenses/mit-license
+ */
+import React, { Component } from 'react'
+import Globe from 'worldwind-react-globe'
+import { 
+  CardColumns, 
+  Container } from 'reactstrap'
+import { 
+  LayersCard, 
+  MarkersCard, 
+  NavBar, 
+  NavBarItem, 
+  SearchBox, 
+  SettingsCard, 
+  Tools } from 'worldwind-react-globe-bs4'
+
+import './App.css'
+
+/* global WorldWind */
+
+export default class App extends Component {
+    
+  constructor(props) {
+    super(props)
+    this.state = {
+      lat: 34.2,
+      lon: -119.2,
+      alt: 10e6,
+      globe: null
+    }
+    // Set the location of the WorldWind "images" folder to the root of the public folder
+    Globe.setBaseUrl("./");    
+    
+    this.globeRef = React.createRef()
+    this.layersRef = React.createRef()
+    this.markersRef = React.createRef()
+    this.settingsRef = React.createRef()
+  }
+  
+  componentDidMount() {
+    let globe = this.globeRef.current
+
+    // Important! Trigger a state change after the globe is mounted
+    this.setState({globe: globe})
+    
+    // Get the user KML
+    // TODO: obtain from https://inreach.garmin.com/feed/ShareLoader/<user>
+    let kml = './feeds/feed-BruceSchubert.kml'
+    let layer = globe.addLayer('renderables', {category: 'overlay', enabled: true, displayName: 'Bruce Schubert'})
+
+    // Add the user to the layer
+    let kmlFilePromise = new WorldWind.KmlFile(kml)
+    kmlFilePromise.then(function(kmlFile) {
+      layer.addRenderable(kmlFile);
+    });
+  }
+  
+  render() {
+    
+    const globe = this.globeRef.current
+    
+    const layers = [
+      {layer: 'blue-marble', options: {category: 'base', enabled: false}},
+      {layer: 'blue-marble-landsat', options: {category: 'base', enabled: false}},
+      {layer: 'bing-aerial', options: {category: 'base', enabled: false}},
+      {layer: 'bing-aerial-labels', options: {category: 'base', enabled: false}},
+      {layer: 'eox-sentinal2', options: {category: 'base', enabled: false}},
+      {layer: 'eox-sentinal2-labels', options: {category: 'base', enabled: true}},
+      {layer: 'eox-openstreetmap', options: {category: 'overlay', enabled: false, opacity: 0.8}},
+      {layer: 'bing-roads', options: {category: 'overlay', enabled: false, opacity: 0.8}},
+      {layer: 'renderables', options: {category: 'data', enabled: true, displayName: 'Markers'}},
+      {layer: 'compass', options: {category: 'setting', enabled: false}},
+      {layer: 'coordinates', options: {category: 'setting', enabled: true}},
+      {layer: 'view-controls', options: {category: 'setting', enabled: true}},
+      {layer: 'stars', options: {category: 'setting', enabled: false}},
+      {layer: 'atmosphere-day-night', options: {category: 'setting', enabled: false}}
+    ]
+    
+    const navbarItems = [
+      <NavBarItem key='lyr' title='Layers' icon='list' collapse={this.layersRef.current}/>,
+      <NavBarItem key='mkr' title='Markers' icon='map-marker' collapse={this.markersRef.current}/>,
+      <NavBarItem key='set' title='Settings' icon='cog' collapse={this.settingsRef.current}/>
+    ]
+   
+    const navbarSearch = <SearchBox globe={globe}/>
+    
+    return (
+      <div>
+        <NavBar 
+            logo=''
+            title='Garmin inReach Tracker'
+            href='https://github.com/emxsys/garmin-inreach'
+            items={navbarItems}
+            search={navbarSearch} />
+        <Container fluid className='p-0'>
+          <div className='globe'>
+              <Globe 
+                ref={this.globeRef} 
+                layers={layers}/>
+          </div>
+          <div className='overlayTools noninteractive'>
+              <Tools 
+                globe={globe} 
+                markers={this.markersRef.current}
+                markersLayerName='Markers'/>
+          </div>
+          <div className='overlayCards noninteractive'>
+            <CardColumns>
+              <LayersCard
+                ref={this.layersRef}
+                categories={['overlay', 'base']} 
+                globe={globe} />
+              <MarkersCard
+                ref={this.markersRef}
+                globe={globe}
+                markersLayerName='Markers' />
+              <SettingsCard
+                ref={this.settingsRef}
+                categories={['setting']} 
+                globe={globe} />
+            </CardColumns>
+          </div>
+        </Container>
+      </div>
+    )
+  }
+}
